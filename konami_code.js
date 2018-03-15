b34bb806a5a6a94c78b0b03e8d1f66b761a5bcc9
William Chabla <w_chabla@hotmail.com>
2018-03-15 17:59:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,14 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let body = document.querySelector('body');
+  
+  function checkCode(e){
+    
+  }
+  
+  body.addEventListener('keyDown', checkCode);
 
-}
+} 

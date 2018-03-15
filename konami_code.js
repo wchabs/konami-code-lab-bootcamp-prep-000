9f11eafc5a1d6b36dc68dea653c66804a43a6b93
William Chabla <w_chabla@hotmail.com>
2018-03-15 18:50:10 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-let index = 0;
+//let index = 0;
 
 function init() {
+  
+  let index = 0;
+  
   let body = document.querySelector('body');
   body.addEventListener('keyDown', checkCode);
   
   function checkCode(e){
     const key = parseInt(e.detail || e.which);
+    
     if (key === code[i]) {
       index++;
       
